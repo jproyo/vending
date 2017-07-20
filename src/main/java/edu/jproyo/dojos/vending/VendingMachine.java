7b8c88a6ae88f825ed10e11e4a3ae78b9a5f70c0
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-07-20 11:14:04 +0200

    Adding travis-ci support

--- src/main/java/edu/jproyo/dojos/vending/VendingMachine.java
+++ src/main/java/edu/jproyo/dojos/vending/VendingMachine.java
@@ -3,21 +3,50 @@
  */
 package edu.jproyo.dojos.vending;
 
+import edu.jproyo.dojos.vending.machine.VendingMachineBuilder;
 import edu.jproyo.dojos.vending.model.OrderResult;
 import edu.jproyo.dojos.vending.model.ProductOrder;
 import edu.jproyo.dojos.vending.model.ProductRequest;
 import edu.jproyo.dojos.vending.model.ResetStatus;
 
 /**
- * @author juan
+ * The Interface VendingMachine.
  *
+ * @author juan
  */
 public interface VendingMachine {
 	
+	/**
+	 * Select.
+	 *
+	 * @param request the request
+	 * @return the product order
+	 */
 	public ProductOrder select(ProductRequest request);
 	
+	/**
+	 * Cancel.
+	 *
+	 * @param order the order
+	 * @return the order result
+	 */
 	public OrderResult cancel(ProductOrder order);
 	
+	/**
+	 * Reset.
+	 *
+	 * @return the reset status
+	 */
 	public ResetStatus reset();
+	
+	
+	/**
+	 * Creates the.
+	 *
+	 * @return the vending machine
+	 */
+	default VendingMachine create(){
+		return new VendingMachineBuilder().build();
+	}
 
 }
---
+++ src/main/java/edu/jproyo/dojos/vending/machine/MachineImpl.java
@@ -0,0 +1,29 @@
+package edu.jproyo.dojos.vending.machine;
+
+import edu.jproyo.dojos.vending.VendingMachine;
+import edu.jproyo.dojos.vending.model.OrderResult;
+import edu.jproyo.dojos.vending.model.ProductOrder;
+import edu.jproyo.dojos.vending.model.ProductRequest;
+import edu.jproyo.dojos.vending.model.ResetStatus;
+
+public class MachineImpl implements VendingMachine {
+
+	@Override
+	public ProductOrder select(ProductRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OrderResult cancel(ProductOrder order) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResetStatus reset() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
